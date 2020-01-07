f3258190b55a92db489daf7734448824a482f8aa
Henry Cooke <me@prehensile.co.uk>
2020-01-07 22:10:35 +0000

    added loglevel to config

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
+import logging
+
 FRAME_INTERVAL = 1.0 / 18.0
 MEDIA_FILE = "media/rick.mp4"
 MAX_CLIENTS = 4
 CAPACITY_MESSAGE = "Maximum number of connections reached ({}). This is only a tiny VPS! Please try later.".format( MAX_CLIENTS )
 SERVER_PORT = 2020
+LOG_LEVEL = logging.INFO
